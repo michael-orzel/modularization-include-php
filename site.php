13e317ebf8611586f171557fc1b0da053acc6ff1
Michael Orzel <56739010+michael-orzel@users.noreply.github.com>
2020-10-14 14:42:14 -0400

    Create site.php

---
+++ site.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+        <?php
+            $title = "Blog Post Title";
+            $author = "Goomba Blogger";
+            $wordCount = 340;
+            include "article-header.php";
+
+            include "useful-tools.php";
+            echo "<br>";
+            sayHi("Mickey Mouse");
+            echo "<br>";
+            echo $feetInMiles;
+        ?>
+
+    </body>
+</html>
